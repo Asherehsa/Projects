71a7318f212cd7f80383bf489435d9032538a6e3
Asher <ashersilb@gmail.com>
2014-02-12 09:44:34 -0800

    Deleted class files

--- Exercise01_08.java
+++ Exercise01_08.java
@@ -2,30 +2,35 @@
 // Asher Silberman
 // COMP 110/L 1100-1345
 /* The program I am submitting is my own work.   I may have had some help learning how to do  it, but ultimately, I wrote and tested this entire exercise myself. */
-/* area and perimeter */
+/* Print area given a radius */
+
+import java.util.Scanner;
+
 public class Exercise01_08 {
-public static void main (String[] args){
-double radius = 5.5;
-double perimeter = 2 * radius * Math.PI;
+{
+public static void main (String[] args)
+{
+Scanner input = new Scanner(System.in);
+int radius = input.nextInt();
 double area = radius * radius * Math.PI;
-System.out.println(area + " " + perimeter);
+System.out.println(area);
 }}
-/*https://github.com/Asherehsa/Projects/blob/master/Exercise01_08.java
- ▄▄▄▄▄▄▄  ▄    ▄  ▄▄▄▄▄ ▄  ▄▄▄▄▄▄▄
- █ ▄▄▄ █ ▀█▄█▀ ▄█   ▄██▄▄▄ █ ▄▄▄ █
- █ ███ █ ▀▀▄▀▄▄█▀▀▀▄█   ▄  █ ███ █
- █▄▄▄▄▄█ █ █ ▄▀█▀█ █ ▄ █ █ █▄▄▄▄▄█
- ▄▄▄▄▄ ▄▄▄▄▄ █▄▀█▀▄ ▀ ██▄█▄ ▄ ▄ ▄ 
- ▀█ ▄█▄▄▀██ ▄▄ ▀▄█▄▄▀██▀▀  ▀▄▄▄▀█▀
- ▄ ▀███▄▄██▀█ ▄ ▄▄▄ █▀▀▄ ▄█▀▄█▄▀  
- ▄▄ ▄ █▄█  ▀ ▀▀▀▄█  ▀▀▀▀▀▄▄██▄  █▀
- █▄██▀▀▄█▄▄▀ █▄ ▀ ▄▀▄▀▀▄▀▀█▀█▄ ▀▄ 
- ▀▀▄▄▀█▄  ▄ ▄▄█▄▄█ ▀▀▀ ▄▀ ▄█  █ █▀
- ▄█ ▄▀█▄▄▀ ▄█ ▄▄▄ ▄ ▀ ▀█ ▄▀  ▄ ▀▄ 
- █▀█▀  ▄ ▄██ ▀█ ▄▀▄▄▀█  ▀▄▄█▄ █ █▀
- █ ▄█ ▀▄▄▄▄█ █▄▄▄▄▄██▀▀▄▄█████▀▀ ▄
- ▄▄▄▄▄▄▄ ██▀▄▄  ▄██▀▀▄█▄▄█ ▄ ██▀▄▀
- █ ▄▄▄ █ ▄ ▀█▄▄██▀  ▀▄▀█▀█▄▄▄█▀▀▄ 
- █ ███ █ █▄█ ▀▄█▄▀ ▀▀▀▀ ▀▄▀ ▀▀▄▄▄ 
- █▄▄▄▄▄█ █▄▀ █▄▄▀▄▄  █▀██▄▄ ▀ █▀▄ 
+/*https://github.com/Asherehsa/Projects/blob/master/Excercise01_08.java
+ ▄▄▄▄▄▄▄    ▄▄▄  ▄ ▄  ▄▄   ▄▄▄▄▄▄▄
+ █ ▄▄▄ █  ▀▀ ▄  ▄█  ▄█▄▄▄▄ █ ▄▄▄ █
+ █ ███ █ █▄  ▀▀█ █▀▄█▀▀▀▄  █ ███ █
+ █▄▄▄▄▄█ ▄ ▄▀█ ▄ █ █ ▄▀█ █ █▄▄▄▄▄█
+ ▄▄   ▄▄▄ ▄▀█  ██▄▄ ▀ ▄█▄█   ▄▄   
+ ▄ ▄▀▀▄▄ ▀▀▄ ▀█▄█▄▄▄▀▄▄ ▀   ██▄▀█ 
+  ██▀ ▀▄ ██ ▄█ █ ▄  ▀█  ▄▄▀██▀ ▀▄▄
+ █▀█▄▄▄▄█▄▀▄▄▄ ▀██  ▀▀ ▀▀▄▄█▄▄  █▀
+ ██▄█ █▄▄▄▀ █ ▄▀█▀▄▀▄  █▀▀█ ▄█ ▀▄▀
+  ▀▀▀ ▀▄ ▀ ▀▄▀████▄▀██▀ █  ▀▀▄▀ ▀█
+  ▄█▀▄▀▄▀ ▄  █▄ █▄▄ ▀  █ ▄▀ ▀▄ ▀▄ 
+ █▀ ▀ █▄▀█▀▀▄▄███▄▄▄▀▄▀▀▀▄▄▄█▀█ █ 
+ █▄ ▀▀█▄▀█▀█▀▄  ▄█ █▀█   ███▄██▀▄ 
+ ▄▄▄▄▄▄▄ ██▀ ▀█ ███▀▀▄▄▄▄█ ▄ ██▀▄▀
+ █ ▄▄▄ █ ▀▄▄ █▄██▀  ▀█ ▄▀█▄▄▄█▀▀█▀
+ █ ███ █   ██▄▄▄██▄▀██ ▄█▄█▄ █ ▄ ▄
+ █▄▄▄▄▄█ █▄ ▀ ▄█ ▄▄  █ ██▄▄   █▀▄ 
 */
--- Exercise01_09.java
+++ Exercise01_09.java
@@ -1,3 +1,8 @@
+//1.09 - 1/28
+// Asher Silberman
+// COMP 110/L 1100-1345
+/* The program I am submitting is my own work.   I may have had some help learning how to do  it, but ultimately, I wrote and tested this entire exercise myself. */
+/* finds area and perimeter given width and height */
 public class Exercise01_09 {
 public static void main (String[] args){
 double w = 4.5;
