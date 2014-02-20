e8c0f2af83b389cb3a5f33301b387bb37770f133
Asher <asherehsa@gmail.com>
2014-02-19 21:47:26 -0800

    End of class

---
+++ Exercise03_17.java
@@ -0,0 +1,40 @@
+//3.17 - 2/19
+// Asher Silberman
+// COMP 110/L 1100-1345
+/* The program I am submitting is my own work.   I may have had some help learning how to do  it, but ultimately, I wrote and tested this entire exercise myself. */
+/* Rock Paper Scissors */
+
+import java.util.*;
+public class Exercise03_17  {
+	public static void main(String [] args)
+	{
+	//generate a random num
+	int rand = (int) (Math.random()*3);
+   System.out.println("rundom numbar " + rand);
+//0 = rock 1 = paper 2 = scissors
+	System.out.println("Rock, Paper or Scissors? ");
+   Scanner input = new Scanner(System.in);
+   String guess = input.nextLine().toLowerCase();
+	
+   switch (guess) {
+      case "rock":
+      if (rand == 0){
+	  System.out.println( guess +" vs rock: it is a tie!");
+	  }
+	  else if (rand == 1){
+	  System.out.println( guess +" vs paper: you loose!");
+	  }
+	  else
+	  System.out.println( guess +" vs scissors: you win!");
+      break;
+      case "paper": 
+	  if (rand == 0)
+	  System.out.println( guess +" vs rock: you win!");
+      break;
+//      case "scissors": ;
+//      break;
+   }
+	//Parse lottery number into individual digits
+	ArrayList ln = new ArrayList(); 
+	}
+}
--- notes.java
+++ notes.java
@@ -21,39 +21,3 @@ else
 	else
 	d=b;
 
-import java.util.*;
-public class LotteryInLab()
-{
-	public static void main(String [] args)
-	{
-	//Randomly generate lottery number
-	int lottery = (int) (Math.random());
-	
-	//Display lottery number
-	System.out.println("The Lotto # is" + lottery);
-	//Parse lottery number into individual digits
-	ArrayList ln = new ArrayList(); 
-	al.add(lottery/100);
-	al.add((lottery%100)/10);
-	al.add((lottery%10);
-	List list = new ArrayList();
-	//Ask user to enter a guess
-		System.out.println("Please enter three digits");
-		Scanner input = new Scanner(System.in);
-		int guess = input.nextInt();
-	//Parse the guess into its digits
-	int g1 = guess/100;
-	int g2 = (guess%100)/10;
-	int g3 = guess%10;
-	//Did user win $10,000
-	if guess == lottery {
-	System.out.println("You won 10k!")
-	
-	//Did user win $3,000
-	
-	//Did user win $1,000
-	
-	//Let the user know they didn't win anything
-	}
-
-}
