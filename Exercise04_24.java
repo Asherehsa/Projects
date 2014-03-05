e7cf97d4b445cbdd3fd1877a30c2e7146a3ad80a
Asher <asherehsa@gmail.com>
2014-03-05 00:17:50 -0800

    4-24

--- Exercise04_24.java
+++ Exercise04_24.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by Asher on 3/4/14.
  */
 public class Exercise04_24 {
-}
+    /**MainMethod**/
+    public static void main (String[] args) {
+        //Create Scanner
+        Scanner input = new Scanner(System.in);
+        //Read in values
+        System.out.print("Enter the 1\uFB06 city: ");
+        String first = input.nextLine();
+        System.out.print("Enter the 2nd city: ");
+        String second = input.nextLine();
+        System.out.print("Enter the 3rd city: ");
+        String third = input.nextLine();
+        ArrayList<String> list = new ArrayList<String>();
+        list.add(first);
+        list.add(second);
+        list.add(third);
+        java.util.Collections.sort(list);
+        System.out.println(list);
+    }}
