207ebbb8cce7c633be5e949b24fc9280a483350e
Asher <asherehsa@gmail.com>
2014-03-10 11:28:38 -0700

    4-21

---
+++ Exercise04_21.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by Asher on 3/9/14.
+ */
+public class Exercise04_21 {
+    /**MainMethod**/
+    public static void main (String[] args) {
+    //Create Scanner
+    Scanner input = new Scanner(System.in);
+        char character = 1;
+        int isdigit;
+        int notdigit;
+
+    //Read in values
+    System.out.print("Enter a Social Security numbar: ");
+    String num = input.nextLine();
+    for (int i = 0; i <= 10; i++) {
+if (num.charAt(i)).isDigit());
+
+    }
+}}
