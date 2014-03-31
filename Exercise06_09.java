233d404d34861db27dee1a0042f5c70ee0f95c29
Asher <asherehsa@gmail.com>
2014-03-30 20:47:01 -0700

    6-9

--- Exercise06_09.java
+++ Exercise06_09.java
@@ -2,10 +2,25 @@
  * Convert between feet and meters
  */
 public class Exercise06_09 {
-    public static double footToMeter(double foot){
 
+
+    public static double footToMeter(double foot){
+        foot = foot * 0.305;
+        return foot;
     }
     public  static double meterToFoot(double meter){
+        meter = meter * 3.279;
+        return meter;
+    }
+    public static void main(String[] args){
+        double f = 1;
+        double m = 20;
+        System.out.print( "feet \t" + "meters" + "\t | \t meters \t" + "feet \n");
+        do{
+        System.out.printf( "%.2f   %5.3f    |        %5.2f         %5.3f %n", f, footToMeter(f), m, meterToFoot(m));
+        f++;
+        m = m + 5;
+        }while(f <= 10 && m <= 65 );
 
     }
 }
